62ce19528744bf6530b4e93db9edd09046deea20
salvosanzone <salvosansone.ss@gmail.com>
2022-05-07 15:59:54 +0200

    creazione component per il bottone per l'aggiunta dei post

---
+++ social_app/lib/components/add_post_button.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/api/api_post.dart';
+import 'package:social_app/models/user.dart';
+import 'package:social_app/models/post.dart';
+
+class AddPostButton extends StatefulWidget {
+  const AddPostButton({Key? key}) : super(key: key);
+
+  @override
+  _AddPostButtonState createState() => _AddPostButtonState();
+}
+
+class _AddPostButtonState extends State<AddPostButton> {
+
+  late Post _post;
+  late String _text;
+  late String _tag;
+  late TextEditingController _textEditingControllerText;
+  late TextEditingController _textEditingControllerTag;
+
+
+  @override
+  void initState() {
+    _textEditingControllerText = TextEditingController();
+    _textEditingControllerTag = TextEditingController();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        elevation: 20,
+        child: const Icon(Icons.add, color: Colors.white,),
+        onPressed: () async {
+          var popResult = await showModalBottomSheet(
+              context: context,
+              isScrollControlled : true,
+              builder: (context) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                      top: 16, left: 16, right: 16, bottom: MediaQuery.of(context).viewInsets.bottom
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        controller: _textEditingControllerText,
+                        onChanged: (value) {
+                          _text = _textEditingControllerText.text;
+                        },
+                        decoration: const InputDecoration(
+                            hintText: 'Scrivi un testo al post',
+                            icon: Icon(Icons.create)
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: AspectRatio(
+                          aspectRatio: 16 / 9,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black54,
+                                  blurRadius: 16,
+                                  offset: Offset(4, 4), // Shadow position
+                                ),
+                              ],
+                              image: const DecorationImage(
+                                opacity: 0.9,
+                                image: NetworkImage('http://www.blackelk.it/wp-content/uploads/2017/07/animali-selvatici-nelle-citt%C3%A0-BIG.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        controller: _textEditingControllerTag,
+                        onChanged: (value) {
+                          _tag = _textEditingControllerTag.text;
+                        },
+                        decoration: const InputDecoration(
+                            hintText: 'Aggiungi dei hastag',
+                            icon: Icon(Icons.tag)
+                        ),
+                      ),
+                      Row(
+
+                        children: [
+                          TextButton(
+                            child: const Text('Annulla'),
+                            onPressed: () {
+                              _textEditingControllerTag.clear();
+                              _textEditingControllerText.clear();
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('Pubblica'),
+                            onPressed: () async {
+                              User _userOne = const User(
+                                id: "60d0fe4f5311236168a109ca",
+                              );
+                              _post = Post(
+                                image: 'http://www.blackelk.it/wp-content/uploads/2017/07/animali-selvatici-nelle-citt%C3%A0-BIG.jpg',
+                                text: _text,
+                                tags: _tag.split(', '),
+                                owner: _userOne,
+                              );
+                              if(_post == null) {
+                                Navigator.of(context).pop();
+                              }
+                              await ApiPost.addPost(_post);
+                              _text = '';
+                              _tag = '';
+                              _textEditingControllerText.clear();
+                              _textEditingControllerTag.clear();
+                              Navigator.of(context).pop(true);
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }
+          );
+        }
+    );
+  }
+}
--- social_app/lib/pages/home.dart
+++ social_app/lib/pages/home.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:social_app/api/api_post.dart';
+import 'package:social_app/components/add_post_button.dart';
 import 'package:social_app/components/container_post.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:social_app/models/user.dart';
 import 'package:social_app/pages/profile_page.dart';
-import 'package:social_app/models/post.dart';
-import 'package:social_app/models/user.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 
@@ -13,30 +10,19 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 class Home extends StatefulWidget {
   final bool active;
   final String? userId;
-  final Function? callBackFunction;
-  const Home({this.callBackFunction, this.active = true, this.userId, Key? key}) : super(key: key);
+  const Home({this.active = true, this.userId, Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  final navigationKey = GlobalKey<CurvedNavigationBarState>();
-  // creo una variabile che conterrà l'id
-  late Post _post;
-  late String _text;
-  late String _tag;
-  late TextEditingController _textEditingControllerText;
-  late TextEditingController _textEditingControllerTag;
+
   int _currentIndex = 0;
-
-
 
 
   @override
   void initState() {
-    _textEditingControllerText = TextEditingController();
-    _textEditingControllerTag = TextEditingController();
 
     super.initState();
   }
@@ -112,108 +98,7 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 20,
-        child: const Icon(Icons.add, color: Colors.white,),
-        onPressed: () async {
-          var popResult = await showModalBottomSheet(
-            context: context,
-              isScrollControlled : true,
-              builder: (context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                    top: 16, left: 16, right: 16, bottom: MediaQuery.of(context).viewInsets.bottom
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: _textEditingControllerText,
-                      onChanged: (value) {
-                        _text = _textEditingControllerText.text;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: 'Scrivi un testo al post',
-                        icon: Icon(Icons.create)
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black54,
-                                blurRadius: 16,
-                                offset: Offset(4, 4), // Shadow position
-                              ),
-                            ],
-                            image: const DecorationImage(
-                              opacity: 0.9,
-                              image: NetworkImage('http://www.blackelk.it/wp-content/uploads/2017/07/animali-selvatici-nelle-citt%C3%A0-BIG.jpg'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      controller: _textEditingControllerTag,
-                      onChanged: (value) {
-                        _tag = _textEditingControllerTag.text;
-                      },
-                      decoration: const InputDecoration(
-                          hintText: 'Aggiungi dei hastag',
-                          icon: Icon(Icons.tag)
-                      ),
-                    ),
-                    Row(
-
-                      children: [
-                        TextButton(
-                          child: const Text('Annulla'),
-                          onPressed: () {
-                            _textEditingControllerTag.clear();
-                            _textEditingControllerText.clear();
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        TextButton(
-                          child: const Text('Pubblica'),
-                          onPressed: () async {
-                            User _userOne = const User(
-                              id: "60d0fe4f5311236168a109ca",
-                            );
-                            _post = Post(
-                              image: 'http://www.blackelk.it/wp-content/uploads/2017/07/animali-selvatici-nelle-citt%C3%A0-BIG.jpg',
-                              text: _text,
-                              tags: _tag.split(', '),
-                              owner: _userOne,
-                            );
-                            if(_post == null) {
-                              Navigator.of(context).pop();
-                            }
-                            await ApiPost.addPost(_post);
-                            _text = '';
-                            _tag = '';
-                            _textEditingControllerText.clear();
-                            _textEditingControllerTag.clear();
-                            Navigator.of(context).pop(true);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }
-          );
-        }
-    ),
+      floatingActionButton: AddPostButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
 
@@ -243,31 +128,39 @@ class _HomeState extends State<Home> {
               label: 'PROFILE',
             icon: IconButton(
               padding: EdgeInsets.zero,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context, MaterialPageRoute(
+                  builder: (context) {
+                    return ProfilePage(giveMeId: "60d0fe4f5311236168a109ca");
+                  }
+                ),
+                );
+              },
               icon: Icon(Icons.person,),
             )
           ),
         ],
       ),
       /*bottomNavigationBar: CurvedNavigationBar(
-        key: navigationKey,
-        backgroundColor: Colors.transparent,
-        height: 60,
-        color: Colors.orange.shade500,
-        items: [
-          IconButton(
-            icon: const Icon(Icons.add_circle, color: Colors.white,),
-            onPressed: () {
-              final navigationState = navigationKey.currentState!;
-              navigationState.setPage(0);
-            },
-          ),
-          const Icon(Icons.add_circle, color: Colors.white,),
-          const Icon(Icons.home, color: Colors.white,),
-          //const Icon(Icons.person, color: Colors.white,),
-        ],
-      ),
-*/
+          key: navigationKey,
+          backgroundColor: Colors.transparent,
+          height: 60,
+          color: Colors.orange.shade500,
+          items: [
+            IconButton(
+              icon: const Icon(Icons.add_circle, color: Colors.white,),
+              onPressed: () {
+                final navigationState = navigationKey.currentState!;
+                navigationState.setPage(0);
+              },
+            ),
+            const Icon(Icons.add_circle, color: Colors.white,),
+            const Icon(Icons.home, color: Colors.white,),
+            //const Icon(Icons.person, color: Colors.white,),
+          ],
+        ),
+      */
 
 
     );
